fe0c58075d8e497968aa315e7b37454b87818e38
Mjklike <1840781936@qq.com>
2019-04-01 15:51:44 +0800

    框架搭建完成0.11

---
+++ src/main/java/com/neuedu/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.neuedu.service.impl;
+
+import com.neuedu.pojo.User;
+import com.neuedu.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public int add(User user) {
+        return 0;
+    }
+}
